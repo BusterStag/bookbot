45aede8f53294f6d4d3e48ba1623517992546d01
generousmouse40 <ryanlisenby@msn.com>
2025-01-09 22:13:43 -0800

    Finalize bookbot

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_dict = letter_count(text)
-    chars_sorted_list = chars_dict_to_sorted_list(char_dict)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
     print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
@@ -34,21 +34,20 @@ def chars_dict_to_sorted_list(num_chars_dict):
     return sorted_list
 
 
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-        
-
-def letter_count(text):
-    letters = dict()
-    for letter in text:
-        letter = letter.lower()
-        if letter in letters:
-            letters[letter] += 1
-        else:
-            letters[letter] = 1
-    print(letters)
 
 
 main()
-
